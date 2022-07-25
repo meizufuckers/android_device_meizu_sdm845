db1c11565a03894a896148e6b13e4b36f31d7274
TRANSAERO21 <martynenkodima21@gmail.com>
2022-07-26 00:24:08 +0300

    sdm845: proprietary: don't repack modem file

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -20,45 +20,6 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-function extract_non_hlos() {
-    RADIO_DIR=$MY_DIR/../../../vendor/$VENDOR/$DEVICE
-    ANDROIDMK=$RADIO_DIR/Android.mk
-    NON_HLOS_PATH=$RADIO_DIR/radio/NON-HLOS.bin
-    EXTRACTED_PATH=$RADIO_DIR/non_hlos
-    if [ -d $EXTRACTED_PATH ]; then
-        rm -rf $EXTRACTED_PATH
-    fi
-    if [ -f $NON_HLOS_PATH ]; then
-        if [ $(stat -c%s $NON_HLOS_PATH) -gt 51199 ]; then
-            if [ ! -z $(command -v 7z) ]; then
-                mkdir -p $EXTRACTED_PATH
-                7z x $NON_HLOS_PATH -o$EXTRACTED_PATH || true
-                cat << EOF >> $ANDROIDMK
-ifeq (\$(LOCAL_PATH)/non_hlos, \$(wildcard \$(LOCAL_PATH)/non_hlos))
-
-PREBUILT_MODEM := \$(PRODUCT_OUT)/NON-HLOS.bin
-ifeq (\$(PREBUILT_MODEM), \$(wildcard \$(PREBUILT_MODEM)))
-
-SCRIPT_DIR := \$(LOCAL_PATH)/../../../device/$VENDOR/$DEVICE_COMMON/radio/scripts
-
-\$(shell \$(SCRIPT_DIR)/fatgen.py --fat16 --name=\$(PREBUILT_MODEM) --size=120 --sectorsize=4096)
-\$(shell \$(SCRIPT_DIR)/fatadd.py --name=\$(PREBUILT_MODEM) --from=non_hlos --sectorsize=4096)
-
-\$(call add-radio-file,\$(PREBUILT_MODEM))
-
-endif
-endif
-
-EOF
-            else
-                echo "Unable to find 7z"
-            fi
-        fi
-    else
-        echo "Unable to find NON-HLOS.bin at $NON_HLOS_PATH"
-    fi
-}
-
 function fixup_filesmap_path() {
     ANDROIDMK=$MY_DIR/../../../vendor/$VENDOR/$DEVICE/Android.mk
     sed -i "s|$DEVICE/radio/filesmap|$DEVICE_COMMON/radio/filesmap|g" $ANDROIDMK
@@ -73,6 +34,5 @@ setup_vendor $DEVICE $VENDOR $ANDROID_ROOT false false
 write_headers $DEVICE
 append_firmware_calls_to_makefiles
 fixup_filesmap_path
-extract_non_hlos
 write_makefiles $MY_DIR/../$DEVICE/proprietary-files-$DEVICE.txt true
 write_footers
